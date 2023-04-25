9c39f6c1a99bb9ca262f9f29f3aab64592cb6cc6
alarmi04 <114494727+alarmi04@users.noreply.github.com>
2023-04-25 15:20:21 +0200

    calculadoraTest

---
+++ test/calculadoraTest.php
@@ -0,0 +1,27 @@
+<?php
+use PHPUnit \Framework\TestCase;
+class Calculadora
+{
+    private $num1;
+    private $num2;
+    public function __construct($a1,$a2)
+    {
+    $this->num1=$a1;
+    $this->num2=$a2;
+    }
+    public function suma(){
+        return $this->num1+$this->num2;
+    }
+}
+?>
+
+
+<!-- public function resta(){
+        if($this->num1>$this->num2)
+            return $this->num1-$this->num2;
+        } else {
+            $this->num2-$this->num1;
+        }
+    public function multiplicacion(){
+        return $this->num1*$this->num2;
+        } -->
